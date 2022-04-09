81946fa90a4392ee6228c31a117d1e1b91764e4a
Marco Santonastasi <84954908+marco-ih-bcn@users.noreply.github.com>
2022-04-09 17:08:00 +0200

    cleaned starter_code

--- 04_code-css_box_model/scripts/script.js
+++
@@ -1,8 +0,0 @@
-console.log("Script loaded.")
-const element = document.querySelector('.red');
-const sizeP = document.getElementById('size')
-const positionInfo = element.getBoundingClientRect();
-const height = positionInfo.height;
-const width = positionInfo.width;
-
-sizeP.textContent = `${width}px ${height}px`;
